12a089f1751e68ec5a030716800a4ec88a80edc1
杨鹏 <1903087169@qq.com>
2023-02-10 15:50:24 +0800

    feat: 复现改变子组件值，父组件不能响应

---
+++ pages/index/childItem.vue
@@ -0,0 +1,19 @@
+<template>
+<div style="border-bottom: 1px solid red">
+    <input v-model="item.name" style="border: 1px solid #333"/>
+    改变这个值，父组件不会跟着改变
+</div>
+</template>
+
+<script>
+export default {
+    name: "childItem",
+    props: {
+        item: Object,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="content">
+        <div v-if="list[0]" style="background: #d8dce1">
+            {{list[0].name}}
+            <input v-model="list[0].name" style="border: 1px solid red">
+        </div>
+		<view v-for="item in list" style="padding: 10px">
+		    <child-item v-model="item.name" :item="item" :name="item.name"></child-item>
+		</view>
+	</view>
+</template>
+
+<script>
+	import ChildItem from "./childItem";
+    export default {
+        components: {ChildItem},
+        data() {
+			return {
+				list: [
+					{
+						name: 'Hello'
+					}
+				]
+			}
+		},
+		onLoad() {
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
